881134223c5d54024ab7fe8f987778ea40d9f7ee
rodrigoaustincascao <rodrigoaustincascao@gmail.com>
2020-06-24 22:33:42 -0300

    bot text wikipedia

---
+++ robots/text.js
@@ -0,0 +1,63 @@
+const algorithmia = require('algorithmia')
+const algorithmiaApiKey  = require('../credentials/algorithmia.json').apiKey
+const sentenceBoundaryDetection = require('sbd')
+
+
+async function robot(content){
+
+  await fetchContentFromWikipedia(content)
+  sanitizeContent(content)
+  breakContentIntoSentences(content)
+
+  async function fetchContentFromWikipedia(content){
+    const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
+    const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
+    const wikipediaResponse = await wikipediaAlgorithm.pipe(content.searchTerm)
+    const wikipediaContent = wikipediaResponse.get()
+    console.log(wikipediaContent)
+
+    content.sourceContentOriginal = wikipediaContent.content
+  }
+
+  function sanitizeContent(content) {
+    const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal)
+    const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
+
+    content.sourceContentSanitized = withoutDatesInParentheses
+
+    function removeBlankLinesAndMarkdown(text) {
+      const allLines = text.split('\n')
+
+      const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
+        if (line.trim().length === 0 || line.trim().startsWith('=')) {
+          return false
+        }
+
+        return true
+      })
+
+      return withoutBlankLinesAndMarkdown.join(' ')
+    }
+  }
+
+  function removeDatesInParentheses(text) {
+    return String(text).replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ')
+  }
+
+  function breakContentIntoSentences(content){
+
+    content.sentences = []
+
+    const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized)
+    sentences.forEach((sentence)=>{
+      content.sentences.push({
+        text: sentence,
+        keyworks: [],
+        images: []
+      })
+    })
+  }
+
+}
+
+module.exports = robot
